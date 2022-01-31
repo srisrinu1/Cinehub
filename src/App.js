19a649b4c0d90886c6dcbcf5e2130ea9339d817a
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-02-01 00:04:51 +0530

    Added a Router and navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import React from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import { Container } from "@material-ui/core";
+import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/MainNav';
 
 import './App.css';
 
 function App() {
   return (
+    <Router>
+      <Header/>
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+       <Container>
+       Movie app
+        <Switch>
+          <Route path="/" component={Trending}/>
+          <Route path="/movies" component={Movies}/>
+          <Route path="/series" component={Series}/>
+          <Route path="/search" component={Search}/>
+        </Switch>
+       </Container>
     </div>
+     <SimpleBottomNavigation />
+    </Router>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return <div>
+       <span className="header"><i class="fas fa-film"></i>Cinehub</span>
+  </div>;
+};
+
+export default Header;
