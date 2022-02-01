55758e605176059edd41a29717ff99d0ff05fddd
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-02-02 00:02:20 +0530

    commit

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -7,8 +7,7 @@ import http from '../../Core/Axios';
 const Trending = () => {
    const [content,setContent] =useState([]);
    const apiKey = process.env.REACT_APP_API_KEY;
-  //  console.log(process.env)
-  //  console.log(apiKey);
+  
    
    const trendingUrl=`trending/all/day`
 
@@ -16,20 +15,11 @@ const Trending = () => {
 
    const fetchTrendingData =async()=>{
      
-    // const {data} = await axios.get(
-    //   `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`
-    // );
-    // console.log(data);
-    // const apiKey = process.env.REACT_APP_API_KEY;
+   
     const {data}=await http.get(trendingUrl,{
       params:{ api_key: apiKey },
     });
     console.log(data);
-    // http
-    //     .get(trendingUrl, {
-    //       params: { api_key: apiKey },
-    //     })
-    //     .then((response) => console.log(response.data))
     
    
    };
