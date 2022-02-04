66a37d48c1769c24833ab0dd51b6ebe11fd362e2
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-02-04 15:31:42 +0530

    commit

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import {env} from '../../config/AppConfig'
+import {Badge} from '@material-ui/core'
+import {env} from '../../config/AppConfig';
+import './SingleContent.css';
+
 
 const SingleContent = (props) => {
-    return <div><img src={`${env.imageUrl}${props.poster}`} alt="" /></div>;
+    return ( <div className="media">
+      <Badge badgeContent={props.vote_average} color={props.vote_average?'primary':'secondary'}/>
+    <img className="poster" src={props.poster?`${env.imageUrl}/${props.poster}`:env.unavailable} alt={props.title} />
+    <b>{props.title}</b>
+    <span className="subTitle">
+        {props.media_type === "tv" ? "TV Series" : "Movie"}
+        <span className="subTitle">{props.date}</span>
+      </span>
+    </div>
+    );
 };
 
 export default SingleContent;
