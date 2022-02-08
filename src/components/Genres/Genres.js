f84a316814a5696d6bda95878356a387abcdd469
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-02-08 19:48:25 +0530

    commit

--- src/components/Genres/Genres.js
+++ src/components/Genres/Genres.js
@@ -1,7 +1,78 @@
-import React from 'react';
+import React,{ useEffect} from 'react';
+import useFetch from '../../hooks/useFetch';
+import { Chip } from "@material-ui/core";
 
 const Genres = (props) => {
-  return <div>Genres</div>;
+
+  const {type,selectedGenres,setSelectedGenres,genres,setGenres,setPage}=props;
+  
+  const genresURL=`genre/${type}/list`
+  const {response}=useFetch(genresURL,`language=en-US`)
+  if(response){
+    var results=response.genres;
+    var pages=response.total_pages;
+  
+  }
+  console.log(results);
+
+  
+ useEffect(() =>{
+  setTimeout(() => {
+    setGenres(results);
+   
+  },0);
+  
+ },[results]);
+ 
+  
+ 
+
+
+  // console.log(selectedGenres);
+
+  const handleAdd=(genre)=>{
+    setSelectedGenres([...selectedGenres, genre]);
+    setGenres(genres.filter((g) => g.id !== genre.id));
+    setPage(1);
+    
+    
+  }
+   
+  const handleRemove=(genre)=>{
+    setSelectedGenres(
+      selectedGenres.filter((selectedGenre) => selectedGenre.id !== genre.id)
+    );
+    setGenres([...genres, genre]);
+    setPage(1);
+    
+  } 
+  return (
+    <div style={{ padding: "6px 0" }}>
+      {selectedGenres && selectedGenres.map((genre) => (
+        <Chip
+          style={{ margin: 2 }}
+          label={genre.name}
+          key={genre.id}
+          color="primary"
+          clickable
+          size="small"
+          onDelete={() => handleRemove(genre)}
+        />
+      ))}
+      {genres && genres.map((genre) => (
+        <Chip
+          style={{ margin: 2 }}
+          label={genre.name}
+          key={genre.id}
+          clickable
+          size="small"
+          onClick={() => handleAdd(genre)}
+        />
+      ))}
+
+   
+    </div>
+  )
 };
 
 export default Genres;
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,41 +1,47 @@
-import React,{useState,useEffect,useCallback} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import http from "../Core/Axios";
 
-const useFetch = (url,param,page) => {
-    const [response,setResponse]=useState(null);
+const useFetch = (url, param, page) => {
+    const [response, setResponse] = useState([]);
+    const [Loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-//   const [isLoading, setIsLoading] = React.useState(false);
-    const apiKey= process.env.REACT_APP_API_KEY;
+    //   const [isLoading, setIsLoading] = React.useState(false);
+    const apiKey = process.env.REACT_APP_API_KEY;
 
-      const URL=url+`?api_key=${apiKey}&${param}`
-        const fetchData = async () => {
-            try {
-              const {data} = await http.get(URL,{
-                 
-              });
-              setResponse(data);
-            
-            } catch (error) {
-              setError(error);
-            }
-          };
-        
-       
-   useEffect(()=>{
-      fetchData();
-   },[URL,apiKey,page])
-  // useEffect(useCallback(()=>{
-  //   fetchData();
-  // },[url,apiKey])
-  // ,[]);       
-       
-        
-   
-   
-  return {
-      response,
-  }
-  
+    const URL = url + `?api_key=${apiKey}&${param}`
+    const fetchData = async() => {
+        setLoading(true);
+        try {
+            const { data } = await http.get(URL, {
+
+            });
+
+            setResponse(data);
+            setLoading(false);
+
+        } catch (error) {
+            setError(error);
+        }
+    };
+
+
+    useEffect(() => {
+        fetchData();
+        return (() => {
+            setResponse([]);
+        })
+
+    }, [URL, apiKey, page, param])
+
+
+
+
+
+    return {
+        response,
+        Loading
+    }
+
 };
 
 export default useFetch;
