bbcab5a3d2d88156cd7744752f55d159106ece20
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-02-10 10:13:29 +0530

    commit

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -2,6 +2,8 @@ import React,{useState,useEffect} from 'react';
 import {TextField,Button,Tab,Tabs} from '@material-ui/core';
 import SearchIcon from "@material-ui/icons/Search";
 import useFetch from '../../hooks/useFetch';
+import SingleComponent from '../../components/SingleContent/SingleContent';
+import CustomPagination from '../../components/Pagination/CustomPagination';
 
 const Search = () => {
   const [type,setType] = useState(0);
@@ -23,8 +25,11 @@ const Search = () => {
       
     },0);
   },[type, page, results, pages]);
+
+  console.log(content);
+  console.log(numOfPages);
   
-  console.log(response);
+ 
   return (<div>
   <div style={{display:'flex'}}>
     <TextField
@@ -55,6 +60,25 @@ const Search = () => {
     <Tab  style={{ width: "50%" }}  label="Search TV Series"/>
 
   </Tabs>
+    <div className="trending">
+      {content&& content.map((c)=>(
+        <SingleComponent
+          key={c.id}
+          id={c.id}
+          poster={c.poster_path}
+          title={c.title || c.name}
+          date={c.first_air_date || c.release_date}
+          media_type={type ? "tv" : "movie"}
+          vote_average={c.vote_average}
+        />
+      ))}
+      {searchText && 
+      !content && 
+      (type? <h2>No Movies Found</h2>:<h2>No Series Found</h2>)}
+    </div>
+    {numOfPages > 1 && (
+        <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+      )}
   </div>)
 };
 
