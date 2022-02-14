b08c3069a7b02f653aec6b69c706c9056f75c0bb
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-02-14 20:25:51 +0530

    Made some modifications in content modal

--- src/components/ContentModal/ContentModal.js
+++ src/components/ContentModal/ContentModal.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import Carousel from '../../components/Carousel/Carousel';
+import {env} from '../../config/AppConfig';
+import useFetch from '../../hooks/useFetch';
 import { makeStyles } from '@material-ui/core/styles';
+import { Button } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
+import './ContentModel.css';
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -13,10 +19,10 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     width: "90%",
     height: "80%",
-    backgroundColor: "#495371",
+    backgroundColor: "#F0F0F0",
     border: "1px solid #282c34",
     borderRadius: 10,
-    color: "white",
+    color: "#1A1A40",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(1, 1, 3)
   },
@@ -24,8 +30,15 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ContentModal(props) {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [content,setContent]=useState();
+  const [video,setVideo] = useState();
+  const mediaURL=`${props.media_type}/${props.id}`;
+  const videoURL=`${props.media_type}/${props.id}/videos`;
+  const {response}=useFetch(mediaURL,`language=en-US`);
+  const data=useFetch(videoURL,`language=en-US`);
 
+ 
   const handleOpen = () => {
     setOpen(true);
   };
@@ -33,12 +46,35 @@ export default function ContentModal(props) {
   const handleClose = () => {
     setOpen(false);
   };
+  const fetchData=()=>{
+    setTimeout(()=>{
+      setContent(response);
+    },0);
+    console.log(response);
+   
+   }
+  const fetchVideo = () => {
+    
+     setTimeout(() =>{
+       setVideo(data.response?.results[0]?.key);
+     },0);
+   
+    
+     console.log(video);
+  }
+ 
+  
+    fetchData();
+    fetchVideo();
+   
+    // eslint-disable-next-line
+ 
 
   return (
-    <div>
-      <button type="button" className="media" onClick={handleOpen}>
+    <>
+      <div type="button" className="media" onClick={handleOpen}>
         {props.children}
-      </button>
+      </div>
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
@@ -52,12 +88,65 @@ export default function ContentModal(props) {
         }}
       >
         <Fade in={open}>
-          <div className={classes.paper}>
-            <h2 id="transition-modal-title">Transition modal</h2>
-            <p id="transition-modal-description">react-transition-group animates me.</p>
-          </div>
+        {content && (
+            <div className={classes.paper}>
+              <div className="ContentModal">
+                <img
+                  src={
+                    content.poster_path
+                      ? `${env.imageUrl_500}/${content.poster_path}`
+                      : env.unavailable
+                  }
+                  alt={content.name || content.title}
+                  className="ContentModal__portrait"
+                />
+                <img
+                  src={
+                    content.backdrop_path
+                      ? `${env.imageUrl_500}/${content.backdrop_path}`
+                      : env.unavailable
+                  }
+                  alt={content.name || content.title}
+                  className="ContentModal__landscape"
+                />
+                <div className="ContentModal__about">
+                  <span className="ContentModal__title">
+                    {content.name || content.title} (
+                    {(
+                      content.first_air_date ||
+                      content.release_date ||
+                      "-----"
+                    ).substring(0, 4)}
+                    )
+                  </span>
+                  {content.tagline && (
+                    <i className="tagline">{content.tagline}</i>
+                  )}
+
+                  <span className="ContentModal__description">
+                    {content.overview}
+                  </span>
+
+                  <div>
+                 <Carousel/>
+                  </div>
+
+                  <Button
+                    variant="contained"
+                    startIcon={<YouTubeIcon />}
+                    color="secondary"
+                    target="__blank"
+                    href={`https://www.youtube.com/watch?v=${video}`}
+                  >
+                    Watch the Trailer
+                  </Button>
+                </div>
+              </div>
+            </div>
+          )}
+
         </Fade>
       </Modal>
-    </div>
+    </>
   );
-}
+      }
