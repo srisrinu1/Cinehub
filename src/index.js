e3a9318b81a193aec2e1d0886a57af634105feaa
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-04-17 22:27:59 +0530

    Added authentication with auth0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import "./index.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
+
+console.log(process.env.REACT_APP_CLIENT);
+console.log(process.env.REACT_APP_API_KEY);
 
 ReactDOM.render(
- 
+ <Auth0Provider
+ domain={process.env.REACT_APP_DOMAIN}
+ clientId={process.env.REACT_APP_CLIENT}
+ redirectUri={window.location.origin}
+ cacheLocation="localstorage"
+ >
     <App />
+    </Auth0Provider>
  ,
   document.getElementById('root')
 );
